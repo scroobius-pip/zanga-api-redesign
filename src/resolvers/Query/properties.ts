9466a5bc75435091d8a59b35272d2c21c790433d
scroobius-pip <sim04ful@gmail.com>
2020-09-13 15:03:27 +0100

    updated maxproeprties to 50

--- src/resolvers/Query/properties.ts
+++ src/resolvers/Query/properties.ts
@@ -5,7 +5,7 @@ import mapDbPropertyToProperty from './mapDbPropertyToProperty';
 const propertiesResolver: QueryResolvers.PropertiesResolver = async (_, { input }, ctx) => {
     const { cursor, state, type, budget } = input
 
-    const { data, after, before } = (await ctx.client.properties({ state, type: CostType[type], cursor, size: 25 })).findPropertiesByCostTypeAndState
+    const { data, after, before } = (await ctx.client.properties({ state, type: CostType[type], cursor, size: 50 })).findPropertiesByCostTypeAndState
     const properties = budget ? data.map(mapDbPropertyToProperty).filter(property => property.costValue <= (budget)) : data.map(mapDbPropertyToProperty)
     return {
         properties,
